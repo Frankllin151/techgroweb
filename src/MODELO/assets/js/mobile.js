6a173e46b70b1120778d12510c0c463f7d98331b
Frankllin151 <frankllinsilva300@gmail.com>
2024-08-18 19:20:26 -0300

    third commit

--- src/MODELO/assets/js/mobile.js
+++
@@ -1,5 +0,0 @@
-const btnMobile = document.getElementById("btn-mobile-menu");
-const navMobile = document.querySelector(".navagation-mobile");
-btnMobile.addEventListener("click", () =>{
- navMobile.classList.toggle("active-menu-mobile");
-})
